0158b3eb0127b95510ef9cb638c17f5a547bdfee
Adrian <leonsio192@gmail.com>
2020-12-16 13:39:47 +1100

    initial form

--- src/components/account/edit/Edit.js
+++ src/components/account/edit/Edit.js
@@ -1,28 +1,34 @@
 import { useForm } from 'react-hook-form';
 
+import useOverview from '../overview/useOverview';
+
 // import { ErrorMessage } from '@hookform/error-message';
 
 const Edit = () => {
   const { handleSubmit, register } = useForm();
+  const { account, setAccount } = useOverview();
 
-  const save = (data) => console.log(data);
+  const save = (data) => {
+    console.log(data);
+    setAccount(data);
+  };
 
   return (
     <form onSubmit={handleSubmit(save)}>
       {/* register your input into the hook by invoking the "register" function */}
 
-      <input name="firstName" defaultValue="" ref={register} />
+      <input name="firstName" defaultValue={account.firstName} ref={register} />
 
       {/* include validation with required or other standard HTML validation rules */}
-      <input name="lastName" ref={register} />
+      <input name="lastName" defaultValue={account.lastName} ref={register} />
 
-      <input name="email" defaultValue="" ref={register} />
+      <input name="email" defaultValue={account.email} ref={register} />
 
-      <input name="phone" defaultValue="" ref={register} />
+      <input name="phone" defaultValue={account.phone} ref={register} />
 
-      <input name="dob" defaultValue="" ref={register} />
+      <input name="dob" defaultValue={account.dob} ref={register} />
 
-      <input name="bio" defaultValue="" ref={register} />
+      <input name="bio" defaultValue={account.bio} ref={register} />
 
       <input type="submit" />
     </form>
