2c9688271c522da1a8ea93202311a2b497fd5506
Adrian <leonsio192@gmail.com>
2020-12-21 10:40:44 +1100

    finalise header styling

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -19,6 +19,14 @@ const Header = (props) => {
     root: {
       flexGrow: 1,
     },
+    backLink: {
+      '& a': {
+        textDecoration: 'none',
+        color: 'rgb(0,0,0)',
+        textAlign: 'right',
+        fontSize: '1rem',
+      },
+    },
     headingText: {
       textAlign: 'left',
       color: 'rgb(0,175,149)',
@@ -27,13 +35,32 @@ const Header = (props) => {
         fontSize: '1.5rem',
       },
     },
+    editLink: {
+      '& a': {
+        textDecoration: 'none',
+        color: 'rgb(22,155,213)',
+        textAlign: 'right',
+        fontSize: '1rem',
+      },
+      '& button': {
+        outline: 'none',
+        background: 'transparent',
+        border: '1px solid transparent',
+        color: 'rgb(22,155,213)',
+        textAlign: 'right',
+        fontSize: '1rem',
+        '&:hover': {
+          cursor: 'pointer',
+        },
+      },
+    },
   }));
 
   const classes = useStyles();
 
   return (
     <Grid container direction="row" justify="center" alignItems="center">
-      <Grid>
+      <Grid className={classes.backLink}>
         {editMode && (
           <a href="#" onClick={(e) => handleClick(e, editMode)}>
             <ArrowBackIosIcon />
@@ -43,14 +70,14 @@ const Header = (props) => {
       <Grid xs={9}>
         <h1 className={classes.headingText}>My Account</h1>
       </Grid>
-      <Grid>
+      <Grid className={classes.editLink}>
         {!editMode ? (
           <a href="#" onClick={(e) => handleClick(e, editMode)}>
             Edit
           </a>
         ) : (
           <button href="#" type="submit" form="hook-form">
-            save
+            Save
           </button>
         )}
       </Grid>
